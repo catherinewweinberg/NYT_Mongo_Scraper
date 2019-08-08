006d2014322907c35ec61c99adc85b90634b9074
Catherine Weinberg <catherinewweinberg@gmail.com>
2019-08-08 10:09:12 -0400

    added routes folder, css, added code to index.handlebars, and main.handlebars

---
+++ Routes/routes.js
@@ -0,0 +1,46 @@
+// Routes
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $("article h2").each(function(i, element) {
+      var result = {};
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+// Getting all articles from the DB
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body).then(function(dbNote) {
+    return db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        note: dbNote._id
+      },
+      { new: true }
+    );
+  });
+});
